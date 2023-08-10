fb3a883aca76dfd7292e1e7e005d8a7e239759bc
praveen-agiratech <praveen.m@agiratech.com>
2023-08-10 19:01:43 +0530

    cart products redux doesn't go on refresh

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  items: [], // Array to hold the products in the cart
+    items: JSON.parse(localStorage.getItem('cart')) || [], // Array to hold the products in the cart
 };
 
 const cartSlice = createSlice({
@@ -19,10 +19,12 @@ const cartSlice = createSlice({
         // Otherwise, add the product to the cart with a quantity of 1
         state.items.push({ ...product, quantity: 1 });
       }
+      localStorage.setItem('cart', JSON.stringify(state.items));
     },
     removeFromCart: (state, action) => {
       const productId = action.payload;
       state.items = state.items.filter(item => item.id !== productId); // Remove product from cart
+      localStorage.setItem('cart', JSON.stringify(state.items));
     },
     incrementProductCount: (state, action) => {
         const productId = action.payload;
@@ -30,6 +32,7 @@ const cartSlice = createSlice({
         if(product){
             product.quantity += 1;
         }
+        localStorage.setItem('cart', JSON.stringify(state.items));
     },
     decrementProductCount: (state, action) => {
         const productId = action.payload;
@@ -37,9 +40,11 @@ const cartSlice = createSlice({
         if(product && product.quantity > 1){
             product.quantity -= 1;
         }
+        localStorage.setItem('cart', JSON.stringify(state.items));
     },
     clearCart: state => {
       state.items = []; // Clear the entire cart
+      localStorage.removeItem('cart');
     },
   },
 });
